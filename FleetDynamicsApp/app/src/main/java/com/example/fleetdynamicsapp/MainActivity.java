756a08c011659f04532cabf4cdd0cfb000d3ed0e
TeddyTeasdale <55746072+TeddyTeasdale@users.noreply.github.com>
2020-02-11 16:06:42 +0000

    Add files via upload

---
+++ FleetDynamicsApp/app/src/main/java/com/example/fleetdynamicsapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.fleetdynamicsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Jeni work here
+
+
+
+
+
+
+
+
+
+        // Teddys work here
+
+
+
+
+
+
+
+
+
+        //James Work here
+
+
+
+
+
+
+
+
+
+        // Hafsa work
+
+
+
+
+
+
+
+
+
+
+
+
+        // Micheal work here
+
+
+
+    }
+}
